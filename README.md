97b8df5167dea4a87fd3c3b1469eff87addf96ee
Demetris Stavrou <1180929+demestav@users.noreply.github.com>
2023-01-21 09:54:22 +0200

    Initial commit. Basic left/right movement works

---
+++ README.md
@@ -0,0 +1,27 @@
+# Open Retro Skies
+
+A very small game inspired by 1980's top-down arcade shooters. It served as a learning project to familiarize with Godot Game Engine.
+
+By design, the game is extremely basic and has very few features. This way, the chances of me finishing the game are higher.
+
+## Goals
+
+- Single player game
+- Basic 4 way movement
+- Single weapon with no upgrades
+- Single type of enemy
+- One level
+- The level contains waves of enemies
+- The player has lives
+- Keep score
+- Less than 5 sprites
+- Enemy will share the same sprite as the player, maybe different color
+- Basic sound effects
+
+
+## Non-goals
+
+- No high scores
+- No power ups
+- No main menu
+- No music
---
+++ src/player/player.gd
@@ -0,0 +1,95 @@
+extends CharacterBody2D
+@onready var animation_player = $AnimationPlayer
+
+enum State {
+	IDLE,
+	ROLL_RIGHT_1,
+	ROLL_RIGHT_2,
+	ROLL_LEFT_1,
+	ROLL_LEFT_2,
+	LOOP
+}
+
+var state = State.IDLE
+var from_state = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_player.play("Idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	state_logic()
+	state_transition()
+	move_and_slide()
+
+func state_logic():
+	match state:
+		State.IDLE:
+			velocity.x = lerpf(velocity.x, 0, 0.5)
+		State.ROLL_RIGHT_1:
+			velocity.x = lerpf(velocity.x, 50, 0.5)
+		State.ROLL_RIGHT_2:
+			velocity.x = lerpf(velocity.x, 70, 0.7)
+		State.ROLL_LEFT_1:
+			velocity.x = lerpf(velocity.x, -50, 0.5)
+		State.ROLL_LEFT_2:
+			velocity.x = lerpf(velocity.x, -70, 0.7)
+		State.LOOP:
+			pass
+
+func state_transition():
+	match state:
+		State.IDLE:
+			if Input.is_action_pressed("ui_right"):
+				switch_state(State.ROLL_RIGHT_1)
+			elif Input.is_action_pressed("ui_left"):
+				switch_state(State.ROLL_LEFT_1)
+		State.ROLL_RIGHT_1:
+			if not Input.is_action_pressed("ui_right") and not animation_player.is_playing():
+				switch_state(State.IDLE)
+			else:
+				if not animation_player.is_playing():
+					switch_state(State.ROLL_RIGHT_2)
+		State.ROLL_RIGHT_2:
+			if not Input.is_action_pressed("ui_right"):
+				switch_state(State.ROLL_RIGHT_1)
+		State.ROLL_LEFT_1:
+			if not Input.is_action_pressed("ui_left") and not animation_player.is_playing():
+				switch_state(State.IDLE)
+			else:
+				if not animation_player.is_playing():
+					switch_state(State.ROLL_LEFT_2)
+		State.ROLL_LEFT_2:
+			if not Input.is_action_pressed("ui_left"):
+				switch_state(State.ROLL_LEFT_1)
+		State.LOOP:
+			pass
+
+func switch_state(to_state):
+	from_state = state
+	state = to_state
+	enter(from_state, to_state)
+
+func enter(from_state, to_state):
+	match state:
+		State.IDLE:
+			animation_player.play("Idle")
+		State.ROLL_RIGHT_1:
+			if from_state == State.IDLE:
+				animation_player.play("Roll 1")
+			elif from_state == State.ROLL_RIGHT_2:
+				animation_player.play_backwards("Roll 1")
+		State.ROLL_RIGHT_2:
+			animation_player.play("Roll 2")
+		State.ROLL_LEFT_1:
+			if from_state == State.IDLE:
+				animation_player.play("Roll 1")
+			elif from_state == State.ROLL_LEFT_2:
+				animation_player.play_backwards("Roll 1")
+		State.ROLL_LEFT_2:
+			animation_player.play("Roll 2")
+		State.LOOP:
+			pass
